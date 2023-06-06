fa2e5ba66432c840b6f438c4f8a225a9ba478e39
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-06-06 12:52:35 +0200

    add wm aliases

--- darwin/extra.nix
+++ darwin/extra.nix
@@ -11,5 +11,7 @@
   programs.zsh.shellAliases = {
     wmre =
       "sudo launchctl kickstart -k gui/501/org.nixos.yabai && killall skhd && skhd -c /etc/skhdrc >/dev/null 2>&1 &"; # Restart window manager + hotkey daemon
+    wmon = "launchctl load ~/Library/LaunchAgents/org.nixos.yabai.plist";
+    wmoff = "launchctl unload ~/Library/LaunchAgents/org.nixos.yabai.plist";
   };
 }
